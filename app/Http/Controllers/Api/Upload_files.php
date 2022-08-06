a4f4b803bad65088b5e93159320714a0c044dfa4
ghofur123 <gafur.strik@gmail.com>
2022-08-06 09:35:42 +0700

    upload file

--- app/Http/Controllers/Api/Upload_files.php
+++ app/Http/Controllers/Api/Upload_files.php
@@ -18,81 +18,83 @@ class Upload_files extends Controller
     }
     public function show(Request $request)
     {
+        $validation = Validator($request->all(), [
+            'lowongan_id' => 'required'
+        ]);
+        if ($validation->fails()) {
+            return response([
+                'success' => false,
+                'message' => $validation->errors()
+            ]);
+        }
         return response([
             'success' => true,
             'message' => 'valid',
-            'data' => Upload_files_model::where('id_file_uplods', $request->id)->first()
+            'data' => Upload_files_model::where('lowongan_id', $request->id)->first()
         ], 200);
     }
     public function store(Request $request)
     {
-        // $validation = validator($request->all(), [
-        //     'file' => 'required|mimes:png,jpg,jpeg,gif',
-        //     'id_lowongan' => 'required'
-        // ]);
-        // // check validation
-        // if ($validation->fails()) {
-        //     return response([
-        //         'success' => false,
-        //         'message' => $validation->errors()
-        //     ], 400);
-        // }
+        $validation = validator($request->all(), [
+            'file' => 'required|mimes:png,jpg,jpeg,gif',
+            'lowongan_id' => 'required'
+        ]);
+        // check validation
+        if ($validation->fails()) {
+            return response([
+                'success' => false,
+                'message' => $validation->errors()
+            ], 400);
+        }
         // // menyimpan ke valiable $file
-        // $file = $request->file('file');
-        // $fileName = uniqid() . $file->getClientOriginalName();
-        // Upload_files_model::insert([
-        //     'id_lowongan' => $request->id_lowongan,
-        //     'name' => $fileName,
-        //     'ext_name' => $file->getClientOriginalExtension(),
-        //     'type' => $file->getMimeType(),
-        //     'size' => $file->getSize(),
-        //     'path' => $file->getRealPath(),
-        //     'url' => URL::asset('uploads/')
-        // ]);
-        // $file->move('uploads', $fileName);
-        // return response([
-        //     'success' => true,
-        //     'message' => 'data di simpan',
-        // ], 200);
+        $file = $request->file('file');
+        $fileName = uniqid() . $file->getClientOriginalName();
+        Upload_files_model::insert([
+            'lowongan_id' => $request->lowongan_id,
+            'name' => $fileName,
+            'ext_name' => $file->getClientOriginalExtension(),
+            'type' => $file->getMimeType(),
+            'size' => $file->getSize(),
+            'path' => $file->getRealPath(),
+            'url' => URL::asset('uploads/')
+        ]);
+        $file->move('../uploads', $fileName);
+        return response([
+            'success' => true,
+            'message' => 'data di simpan',
+        ], 200);
     }
     public function update()
     {
     }
     public function destroy(Request $request)
     {
-        // $message = array(
-        //     'required' => ':attribute tidak boleh kosong'
-        // );
-        // $validation = validator($request->all(), [
-        //     'id_file_uplods' => 'required'
-        // ], $message);
-        // // check validation
-        // if ($validation->fails()) {
-        //     $db = Upload_files_model::where('id_file_uplods', $request->id_file_uplods)
-        //         ->first();
-        //     // File::delete('uploads/' . $db->name);
-        //     Upload_files_model::where([
-        //         'id_file_uplods' => $request->id_file_uplods
-        //     ])->delete();
-        //     // cek file ada apa tidak di dalam folder
-        //     if (file_exists('uploads/' . $db->name)) {
-        //         unlink('uploads/' . $db->name);
-        //         $response = response([
-        //             'success' => true,
-        //             'message' => 'data berhasil di hapus1'
-        //         ], 200);
-        //     } else {
-        //         $response = response([
-        //             'success' => true,
-        //             'message' => 'data berhasil di hapus'
-        //         ], 200);
-        //     }
-        // } else {
-        //     $response = response([
-        //         'success' => false,
-        //         'message' => $validation->errors()
-        //     ], 400);
-        // }
-        // return $response;
+        $validation = validator($request->all(), [
+            'id' => 'required'
+        ]);
+        // check validation
+        if ($validation->fails()) {
+            return response([
+                'success' => false,
+                'message' => $validation->errors()
+            ], 400);
+        }
+        $db =  Upload_files_model::where('id', $request->id)->first();
+        // File::delete('uploads/' . $db->name);
+        Upload_files_model::where([
+            'id' => $request->id
+        ])->delete();
+        if (file_exists('../uploads/' . $db->name)) {
+            unlink('../uploads/' . $db->name);
+            return response([
+                'success' => true,
+                'message' => 'data berhasil di hapus'
+            ], 200);
+        } else {
+            return response([
+                'success' => true,
+                'message' => 'data gagal di hapus'
+            ], 400);
+        }
     }
 }
